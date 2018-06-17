70cdc39ae91f699248490f57e34877fd7cfdcb79
Jiggs <me762@hotmail.co.uk>
2018-06-17 21:50:24 +0100

    Fixed 2 decimal float using formating function

--- Calculator.py
+++ Calculator.py
@@ -6,6 +6,6 @@ tip_percentage = float(input("What percentage would you like to tip today?"))
 tip_amount = bill_amount * (float(tip_percentage) / 100)
 total_cost = bill_amount + tip_amount
 
-print("Bill Amount: £" + str(bill_amount))
-print("Tip Amount: £" + str(tip_amount))
-print("Total Payable: £" + str(total_cost))
+print("Bill Amount: £" + ("{0:.2f}".format(bill_amount)))
+print("Tip Amount: £" + ("{0:.2f}".format(tip_amount)))
+print("Total Payable: £" + ("{0:.2f}".format(total_cost)))
